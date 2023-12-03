bf9c2baee9f571dfc8bf9f4970ce2a1f7021db61
Anders Bo Rasmussen <anras573@gmail.com>
2023-12-03 17:50:42 +0100

    Finish Day 01 and Day 02.

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,50 @@
 ﻿namespace AdventOfCode;
 
-public class Day01 : BaseDay
+public partial class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {SumOfFirstAndLastDigit()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {SumOfFirstAndLastNumber()}");
+
+    private int SumOfFirstAndLastDigit()
+    {
+        var nums = _input.Select(l =>
+        {
+            var numbers = l.ToCharArray().Where(char.IsDigit).ToArray();
+
+            return int.Parse($"{numbers[0]}{numbers[^1]}");
+        });
+
+        return nums.Sum();
+    }
+    
+    private int SumOfFirstAndLastNumber()
+    {
+        var nums = _input.Select(l =>
+        {
+            var newLine = l
+                .Replace("one", "one1one")
+                .Replace("two", "two2two")
+                .Replace("three", "three3three")
+                .Replace("four", "four4four")
+                .Replace("five", "five5five")
+                .Replace("six", "six6six")
+                .Replace("seven", "seven7seven")
+                .Replace("eight", "eight8eight")
+                .Replace("nine", "nine9nine");
+
+            var numbers = newLine.ToCharArray().Where(char.IsDigit).ToArray();
+
+            return int.Parse($"{numbers[0]}{numbers[^1]}");
+        });
+
+        return nums.Sum();
+    }
 }
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,101 @@
+﻿namespace AdventOfCode;
+
+public partial class Day02 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {SumOfPossibleGames()}");
+
+    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {SumOfLeastPossibleCubes()}");
+
+    private int SumOfPossibleGames()
+    {
+        var blue = 14;
+        var green = 13;
+        var red = 12;
+
+        var sum = 0;
+        
+        foreach (var line in _input)
+        {
+            var splitLine = line.Split(' ');
+            
+            var id = int.Parse(splitLine[1].Remove(splitLine[1].Length - 1));
+            var isPossible = true;
+
+            for (var i = 2; i < splitLine.Length; i += 2)
+            {
+                var num = int.Parse(splitLine[i]);
+                var color = splitLine[i + 1]
+                    .Replace(",", string.Empty)
+                    .Replace(";", string.Empty);
+                
+                if (color is "blue" && num > blue)
+                {
+                    isPossible = false;
+                    break;
+                }
+                else if (color is "green" && num > green)
+                {
+                    isPossible = false;
+                    break;
+                }
+                else if (color is "red" && num > red)
+                { 
+                    isPossible = false;
+                    break;
+                }
+            }
+            
+            if (isPossible)
+            {
+                sum += id;
+            }
+        }
+        
+        return sum;
+    }
+
+    private int SumOfLeastPossibleCubes()
+    {
+        var power = 0;
+        
+        foreach (var line in _input)
+        {
+            var splitLine = line.Split(' ');
+            var blue = 0;
+            var green = 0;
+            var red = 0;
+            
+            for (var i = 2; i < splitLine.Length; i += 2)
+            {
+                var num = int.Parse(splitLine[i]);
+                var color = splitLine[i + 1]
+                    .Replace(",", string.Empty)
+                    .Replace(";", string.Empty);
+                
+                switch (color)
+                {
+                    case "blue" when num > blue:
+                        blue = num;
+                        break;
+                    case "green" when num > green:
+                        green = num;
+                        break;
+                    case "red" when num > red:
+                        red = num;
+                        break;
+                }
+            }
+            
+            power += blue * green * red;
+        }
+
+        return power;
+    }
+}
